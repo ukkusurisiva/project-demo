622db28bf3a43473f103bfc39b26999239c185a0
ukkusurisiva <150415546+ukkusurisiva@users.noreply.github.com>
2023-11-10 11:11:03 +0530

    Add files via upload

---
+++ code 1.py
@@ -0,0 +1,15 @@
+n=int(input("Enter n value:"))
+s=0
+b=n
+while(n!=0):
+    a=n%10
+    f=1
+    while(a!=0):
+        f=f*(a)
+        a=a-1
+    s=s+f
+    n=n//10
+if(b==s):
+    print("strong")
+else:
+    print("not")
